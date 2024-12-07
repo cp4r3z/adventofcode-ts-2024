a92fbe084a164a506afb4b2fe48842f315beff2a
PAREZ Charles <charles.parez@3ds.com>
2024-12-06 19:29:13 -0600

    potential solution

--- src/06/aoc.test.ts
+++ src/06/aoc.test.ts
@@ -8,21 +8,21 @@ const tinput = await Input.tinput(__dirname);
 const tinput2 = await Input.tinput2(__dirname);
 const input = await Input.input(__dirname);
 
-describe(`Day 05`, () => {
-    it('Part 1', async () => {
+describe(`Day 06`, () => {
+    xit('Part 1', async () => {
         const solution = await part1(tinput);
         expect(solution).toBe(41);
     });
 
-    it('Part 1 (Real Input)', async () => {
+    xit('Part 1 (Real Input)', async () => {
         const solution = await part1(input);
         console.log('Part 1 (Real Input)');
         console.log(solution);
     });
 
-    xit('Part 2', async () => {
+    it('Part 2', async () => {
         const solution = await part2(tinput);
-        expect(solution).toBe(0);
+        expect(solution).toBe(6);
     });
 
     xit('Part 2 (Real Input)', async () => {
--- src/06/aoc.ts
+++ src/06/aoc.ts
@@ -1,6 +1,21 @@
 import { XY } from "../common/base/points";
 import { Direction, Grid2D, GridOptions, GridPoint } from "../common/grids/grid";
 
+class Queue extends Array {
+    private _maxLength: number;
+    constructor(max: number) {
+        super();
+        this._maxLength = max;
+    }
+    // shift will remove 0th element
+    enqueue(element: any) {
+        this.push(element);
+        if (this.length > this._maxLength) {
+          //  this.shift();
+        }
+    }
+}
+
 class Guard extends GridPoint {
     // value is the index of Direction.Cardinals
     turn() {
@@ -22,6 +37,9 @@ class Guard extends GridPoint {
         const cardinal = Direction.Cardinals[this.Value];
         const delta = Direction.CardinalToXY.get(cardinal);
         return this.move(delta);
+    }
+    clone() {
+        return new Guard(this.x, this.y, this.Value);
     }
 
     // TODO: override copy!
@@ -69,11 +87,70 @@ class LabMap extends Grid2D {
                 this.guard.turn();
             } else {
                 // Step
-                this.guard.step();                
+                this.guard.step();
             }
         }
 
         return stepsTaken.size;
+    }
+
+    public patrol2() {
+
+        let stepsTaken = new Set<string>();
+        const newObstaclePositions = new Set<string>();
+        const obstacleHistory = [];  // new Queue(3); // Remember the last three obstacles
+        while (this.inBounds(this.guard)) {
+            stepsTaken.add(Grid2D.HashPointToKey(this.guard));
+            // Look
+            const ahead = this.guard.look();
+            const pointAhead: GridPoint = this.getPoint(ahead);
+            if (pointAhead) {
+                // Obstruction
+                this.guard.turn();
+                obstacleHistory.push(pointAhead);
+            } else {
+                // Before you step, look right. Do you have a clear path to the obstacle 3 turns back?
+if (this.guard.x === 4 && this.guard.y ===8){
+    debugger;
+}
+                /// TODO! You have to look back at every 3rd in the history
+                if (obstacleHistory.length>=3){
+                    for (let i = obstacleHistory.length-3; i >=0; i= i - 4) {
+                        // const element = array[i];
+                        const behind3N = obstacleHistory[i];
+                        if ( this.isPathClear(behind3N)) {
+                            newObstaclePositions.add(Grid2D.HashPointToKey(ahead));
+                        }
+                    }
+                }
+                
+
+                // if (stepHistory.length===3 && this.isPathClear(stepHistory[0])) {
+                //     newObstaclePositions.add(Grid2D.HashPointToKey(ahead));
+                // }
+                // Step
+                this.guard.step();
+            }
+        }
+
+        return newObstaclePositions.size; // missing 3,8
+    }
+
+    private isPathClear(target: GridPoint) {
+        const rightGuard = this.guard.clone();
+        rightGuard.turn();
+
+
+        // step until you're out of bounds or you find the obstacle from 3 turns ago.
+        let clear = false;
+        while (this.inBounds(rightGuard)) {
+            rightGuard.step();
+            if (this.getPoint(rightGuard) === target) {
+                clear = true;
+                break;
+            }
+        }
+        return clear;
     }
 }
 
@@ -90,6 +167,6 @@ export const part2 = async (input: string): Promise<number | string> => {
     map.setFromString2D(input);
     map.print();
 
-    const solution = 0;
+    const solution = map.patrol2();
     return solution;
 };
